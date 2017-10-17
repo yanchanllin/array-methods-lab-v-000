2110f82507b186dbece367dcd9e5928962f14047
YanChan  Lin <yonalin1@gmail.com>
2017-10-17 03:55:03 +0000

    automatically backed up by learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -7,17 +7,17 @@ array.sort
 end
 
 def using_reverse(array)
-using_reverse.reverse?(array)
+array.reverse
 end
 
 def using_first(array)
-using_first.first(array)
+array.first
 end
 
 def using_last(array)
-  using_last.last(array)
+  array.last
 end
 
 def using_size(array)
-using_size.size(array)
+array.size
 end
